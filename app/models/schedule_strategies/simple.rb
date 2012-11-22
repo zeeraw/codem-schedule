4de32eb010f0dd5c8495944c7c81154b741ff5f6
Bart Zonneveld <bart@madebyhiro.com>
2012-11-22 14:32:28 +0100

    Moved jobs into simple strategy

--- app/models/schedule_strategies/simple.rb
+++ app/models/schedule_strategies/simple.rb
@@ -3,6 +3,10 @@ module ScheduleStrategies
     def hosts
       Host.with_available_slots
     end
+
+    def self.jobs(limit)
+      Job.scheduled.unlocked.order("priority DESC, created_at ASC").limit(limit)
+    end
   end
 end
 
--- spec/models/schedule_strategies/simple_spec.rb
+++ spec/models/schedule_strategies/simple_spec.rb
@@ -9,5 +9,11 @@ describe ScheduleStrategies::Simple do
     Host.should_receive(:with_available_slots).and_return 'hosts'
     strategy.hosts.should == 'hosts'
   end
+
+
+  it "should return the correct jobs" do
+    Job.stub_chain(:scheduled, :unlocked, :order, :limit).and_return j
+    ScheduleStrategies::Simple.jobs(10).should == j
+  end
 end
 
